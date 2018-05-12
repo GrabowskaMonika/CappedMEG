0c843cf7f591a9d389c88f341b2ea0a44706a570
Monika Grabowska <monika.j.grabowska@gmail..com>
2018-05-12 17:15:49 +0200

    Capping for MEGsmall update

--- CappedMEG/capMEG.m
+++ CappedMEG/capMEG.m
@@ -2,7 +2,6 @@ function [ S, l, capping ] = capMEG(S, k, l, capping, d)
     
 kmax = k+3;
     if(l > kmax)
-%         disp('dziwne')
         rho = 1/(d-kmax);
         S = S - rho*S(kmax);
         S = S(1:kmax);
